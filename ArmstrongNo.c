ddb532646dfd58b333ff65f9bb0e480973d19c6d
Triparna Singha <singhatriparna86@gmail.com>
2023-10-02 20:09:26 +0530

    Create ArmstrongNo.c

---
+++ ArmstrongNo.c
@@ -0,0 +1,19 @@
+/Armstrong Number/
+#include<stdio.h>
+int main()
+{
+	int n,s=0,r,t;
+	printf("Enter a number");
+	scanf("%d",&n);
+	t=n;
+	while (n>0)
+	{
+		r=n%10;
+		s=s+r*r*r;
+		n=n/10;
+	}
+	if (s!=t)
+		printf("Armstrong");
+	else
+		printf("Not Armstrong");
+}
